0a5812540cd0e4d5478f1e69850ecb476b318681
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-04-02 11:48:55 +0200

    Add menu to wp options

--- functions.php
+++ functions.php
@@ -1,5 +1,14 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/assets/scss/theme.css' );
 }
+function registerMenus()
+{
+    register_nav_menus(
+        array(
+            'main' => __('Header'),
+        )
+    );
+}
+add_action('after_setup_theme', 'registerMenus');
