5089aad8f877b583751473395aa4f0e471fea04c
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-04-02 19:17:52 +0200

    Simplify mobile menu

--- header.php
+++ header.php
@@ -9,38 +9,20 @@
 <body>
     <?php wp_body_open(); ?>
     <header class="header">
-        <a href="<?php echo home_url(); ?>">
-            ALEXIA A-J
+        <a class="header__logo" href="<?php echo home_url(); ?>">
+            ALEXIA A-J | WEB DEV
         </a>
         <div class="header__navDesktop">
             <?php wp_nav_menu(array('theme_location' => 'main')); ?>
         </div>
-        <!-- <div class="header__navMobile">
-            <div id="menu_burger" class="nav_burger">
-                <div class="navMobile-top">
-                    <?php
-                    if ( get_theme_mod( 'your_theme_logo' ) ) : ?>
-                        <a href="<?php echo home_url(); ?>">
-                            <img class=header__logo src="<?php echo get_theme_mod( 'your_theme_logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" >
-                        </a>
-                    <?php //
-                    else : ?>
-                        <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-                    <?php endif;
-                    ?>
-                    <a id="closeBtn" href="#" class="close">&times;</a>
-                </div>
-                <?php wp_nav_menu(array('theme_location' => 'main')); ?>
-            </div>
-
-            <a href="#" id="openBtn">
-                <span class="burger-icon">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </span>
-            </a>
-        </div> -->
+        <div class="menuToggle">
+            <span class="bar topBar"></span>
+            <span class="bar middleBar"></span>
+            <span class="bar bottomBar"></span>
+        </div>
+        <div class="header__navMobile">
+            <?php wp_nav_menu(array('theme_location' => 'main')); ?>
+        </div>
     </header>
 
     
