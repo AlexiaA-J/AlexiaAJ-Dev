83500cfe1d8656de59bfe988a8169864381bee9a
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-04-04 22:17:37 +0200

    Fix  burger menu function

--- assets/js/modules/menuBurger.js
+++ assets/js/modules/menuBurger.js
@@ -1,10 +1,12 @@
 function initializeMenuToggle() {
     let menuToggle = document.querySelector(".menuToggle");
+    let header = document.querySelector(".header");
     let headerNavMobile = document.querySelector(".header__navMobile");
     let bars = document.querySelectorAll('.bar');
     if (menuToggle) {
         menuToggle.addEventListener("click", function() {
             headerNavMobile.classList.toggle("active");
+            header.classList.toggle("fixed");
             Array.from(bars).forEach(bar => bar.classList.toggle('active-menu'));
         });
     }
