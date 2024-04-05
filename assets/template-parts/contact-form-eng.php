64d4f1f9dc87274f732c8314bcd3269af5033db1
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-04-05 14:00:19 +0200

    Add modal templates

---
+++ assets/template-parts/contact-form-eng.php
@@ -0,0 +1,2 @@
+<?php
+    echo do_shortcode('[contact-form-7 id="343f7fd" title="Contact Form Eng"]');
---
+++ assets/template-parts/contact-form-fr.php
@@ -0,0 +1,2 @@
+<?php
+    echo do_shortcode('[contact-form-7 id="56253e7" title="Contact Form Fr"]');
