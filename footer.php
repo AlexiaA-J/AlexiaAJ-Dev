bd6307301b8522724e59d62ced8a2d72683a607d
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-04-05 14:06:34 +0200

    Add contact form templates

--- footer.php
+++ footer.php
@@ -1,4 +1,13 @@
         <footer>
+            <div class="modal-overlay">    
+            <?php 
+                $current_url = $_SERVER['REQUEST_URI'];
+                if (strpos($current_url, 'eng') !== false) : ?>
+                <?php get_template_part('assets/template-parts/contact-form-eng'); ?>
+            <?php else: ?>
+                <?php get_template_part('assets/template-parts/contact-form-fr'); ?>
+            <?php endif; ?>
+            </div>
             <p lang="en">© All Rights Reserved</p>
         </footer>
         <?php wp_footer(); ?>
