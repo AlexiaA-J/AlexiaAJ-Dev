fb0e362a0dbd45331de6551df3db89e9bacc8cef
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-04-05 11:59:55 +0200

    Add addIds function

---
+++ assets/js/modules/addIds.js
@@ -0,0 +1,8 @@
+// Add IDs to #linked elements
+
+function initializeAddIds() {
+    const skillsElement = document.querySelector('.skills');
+    if (skillsElement) {
+        skillsElement.setAttribute('id', 'skills');
+    }
+};
