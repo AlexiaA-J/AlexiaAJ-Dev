bd541f0741d30ab1d5be9955d98c83ec88b39b21
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-04-05 14:06:08 +0200

    Add handling of click on menu links

--- assets/js/modules/menuBurger.js
+++ assets/js/modules/menuBurger.js
@@ -2,14 +2,32 @@
 
 function initializeMenuToggle() {
     let menuToggle = document.querySelector(".menuToggle");
-    let header = document.querySelector(".header");
     let headerNavMobile = document.querySelector(".header__navMobile");
+    let header = document.querySelector(".header");
+    let mainContent = document.querySelector(".mainContent");
     let bars = document.querySelectorAll('.bar');
+
     if (menuToggle) {
         menuToggle.addEventListener("click", function() {
+            toggleMenu();
+        });
+
+        // Function to toggle menu and links
+        function toggleMenu() {
             headerNavMobile.classList.toggle("active");
             header.classList.toggle("fixed");
+            mainContent.classList.toggle("active-wrapper");
             Array.from(bars).forEach(bar => bar.classList.toggle('active-menu'));
+        }
+
+        // Add event listener to each menu link to close the menu when clicked
+        let menuLinks = document.querySelectorAll('.header__navMobile .menu-item a');
+        menuLinks.forEach(link => {
+            link.addEventListener('click', function() {
+                if (headerNavMobile.classList.contains("active")) {
+                    toggleMenu();
+                }
+            });
         });
     }
 }
