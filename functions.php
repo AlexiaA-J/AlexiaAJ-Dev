b3496f1a95ae24cb1c9a370724e92dbbdf29704d
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-04-07 16:24:11 +0200

    Add modal script

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/assets/scss/theme.css' );
+    wp_enqueue_script('modal-script', get_template_directory_uri() . '/assets/js/modules/modal.js', array('jquery'), null, true);
     wp_enqueue_script('addIds-script', get_template_directory_uri() . '/assets/js/modules/addIds.js', array('jquery'), null, true);
     wp_enqueue_script('menuBurger-script', get_template_directory_uri() . '/assets/js/modules/menuBurger.js', array('jquery'), null, true);
     wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), null, true);
