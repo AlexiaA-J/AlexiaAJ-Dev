a56c9a97f0855b9f0bd669dbd36b7c7fafbbb5c0
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-04-09 16:37:13 +0200

    Add projects gallery

--- index.php
+++ index.php
@@ -7,7 +7,25 @@
         endwhile; else: ?>
         <p>Sorry, no posts matched your criteria.</p>
     <?php endif; ?>
+        <div class="gallery">
+        <?php
+                $gallery = array(
+                    'post_type' => 'project',
+                    'orderby' => 'date',
+                    'order' => 'DESC',
+                    'post_status' => 'publish',
 
+                );
+
+                $query = new WP_Query($gallery);
+
+                if ($query->have_posts()){
+                    while ($query->have_posts()) : $query->the_post();
+                        get_template_part('assets/template-parts/project');
+                    endwhile;
+                }
+            ?>
+        </div>
     </div>
     <?php
     get_footer();
